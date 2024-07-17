e49469969f429e2612872cb3d98afd9b12d6d402
CarlosHenriqueMatos <86526136+CarlosHenriqueMatos@users.noreply.github.com>
2024-07-16 22:46:47 -0300

    Add files via upload

---
+++ Other/working_with_threads.py
@@ -0,0 +1,28 @@
+from threading import Thread
+# source https://www.youtube.com/watch?v=jkwhht2gXB8&list=PLI4OVrCFuY57b_16D8xs7-hmABHncVD_w&index=4
+def threading_informations():
+    print(Thread.current_thread())
+    print(Thread.current_thread().name)
+    print(Thread.current_thread().ident)
+    print(Thread.current_thread().is_alive())
+class Example:
+    @classmethod
+    def display(self,n,msg):
+        for i in range(n):
+            print(msg)
+    def display_unique(self, x):
+        for i in range(x):
+            print(i)
+
+e1 = Example()
+
+t1 = Thread(target=e1.display,args=(4,"Hello")) #new thread here
+t2 = Thread(target=e1.display_unique,args=(5,))
+t3 = Thread(target=e1.display_unique, kwargs={'x':4})
+t4 = Thread(target=e1.display, kwargs={"n":4,"msg":"Carlos"})
+
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+
